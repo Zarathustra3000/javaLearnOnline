2f5ca8fec001b9f57512a6bb06866a30320048bd
aleksandr <tolastoialeksandr@mail.ru>
2020-08-18 10:00:37 +0300

    start

---
+++ CoreTaskTemplate/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,19 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.*;
+
+public class Util {
+    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/skillbox"+
+            "?verifyServerCertificate=false"+
+            "&useSSL=false"+
+            "&requireSSL=false"+
+            "&useLegacyDatetimeCode=false"+
+            "&amp"+
+            "&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "privetsql";
+
+
+
+}
