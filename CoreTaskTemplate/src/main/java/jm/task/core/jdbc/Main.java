98990f0e12fef8d64e8e7c5469b9286957b49e3a
aleksandr <tolastoialeksandr@mail.ru>
2020-08-22 16:56:39 +0300

    the implementation of the "UserDaoHibenateImpl"

--- CoreTaskTemplate/src/main/java/jm/task/core/jdbc/Main.java
+++ CoreTaskTemplate/src/main/java/jm/task/core/jdbc/Main.java
@@ -7,15 +7,18 @@ import jm.task.core.jdbc.service.UserServiceImpl;
 public class Main {
     public static void main(String[] args) {
 
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("a","b", (byte) 8);
+        userService.saveUser("z","x", (byte) 22);
+        userService.saveUser("c","v", (byte) 13);
 
-//        userService.saveUser("a","b", (byte) 8);
-//        userService.saveUser("z","x", (byte) 22);
-//        userService.saveUser("c","v", (byte) 13);
-//        userService.saveUser("q","w", (byte) 31);
-//        for (User user: userService.getAllUsers()) {
-//            System.out.println(user);
-//        }
-//        userService.cleanUsersTable();
-//        userService.dropUsersTable();
+        for (User user: userService.getAllUsers()) {
+            System.out.println(user);
+        }
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
     }
 }
--- CoreTaskTemplate/src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ CoreTaskTemplate/src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -3,15 +3,11 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
-import java.sql.SQLException;
 import java.util.List;
 
 public class UserDaoHibernateImpl extends Util implements UserDao {
-
-    private Session session = getConnectionHibernate().openSession();
-    //private UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
 
     public UserDaoHibernateImpl() {
 
@@ -20,73 +16,62 @@ public class UserDaoHibernateImpl extends Util implements UserDao {
 
     @Override
     public void createUsersTable() {
-        try {
-            session.createSQLQuery("CREATE TABLE USERS (id long, firstName varchar(45), lastName varchar(45), age int)")
-                    .executeUpdate();
-            session.close();
-        } catch (Exception e) {
-
-        }
+        Session session = getConnectionHibernate().openSession();
+        Transaction transaction = session.beginTransaction();
+        String sql = "CREATE TABLE IF NOT EXISTS users(\n"
+                + "id int NOT NULL AUTO_INCREMENT,\n"
+                + "firstName varchar(45),\n"
+                + "lastName varchar(45),\n"
+                + "age int,\n"
+                + "PRIMARY KEY (id))";
+        session.createSQLQuery(sql).executeUpdate();
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public void dropUsersTable() {
-        try {
-            session.createSQLQuery("DROP TABLE USERS")
-                    .executeUpdate();
-            session.close();
-        } catch (Exception e) {
-
-        }
+        Session session = getConnectionHibernate().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.createSQLQuery("DROP TABLE IF EXISTS users").executeUpdate();
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        User user = new User(name,lastName,age);
-        session.save(user);
-        try {
-            session.getTransaction().commit();
-        } catch (Exception e){
+        Session session = getConnectionHibernate().openSession();
+        session.save(new User(name,lastName,age));
+        session.close();
 
-        }
-        //session.getTransaction().commit();
-        session.clear();
-//        String sql = "INSERT INTO `users` (`name`, `lastname`, `age`) " +
-//                "VALUES ('" + name + "', '" + lastName + "', " + age + ")";
-//        try {
-//            session.createSQLQuery(sql)
-//                    .executeUpdate();
-//            session.close();
-//        } catch (Exception e) {
-//
-//        }
-        //session.createSQLQuery(sql);
     }
 
     @Override
     public void removeUserById(long id) {
-       // userDaoJDBC.dropUsersTable();
+        Session session = getConnectionHibernate().openSession();
+        Transaction transaction = session.beginTransaction();
+        User user = session.load(User.class, id);
+        session.remove(user);
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public List<User> getAllUsers() {
-        String sql = "SELECT * FROM users";
-        return null;
+        Session session = getConnectionHibernate().openSession();
+        List<User> list = session.createQuery("From " + User.class.getSimpleName()).list();
+        session.close();
+        return list;
     }
 
     @Override
     public void cleanUsersTable() {
-        String sql = "DELETE FROM users";
-        session.createSQLQuery(sql);
+        Session session = getConnectionHibernate().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.createQuery("delete from User").executeUpdate();
+        transaction.commit();
+        session.close();
     }
 
-    private void processingSQLQuery(String sqlQuery) {
-//        try {
-//            sessionFactory = getConnectionHibernate().openSession();
-//            statement.executeUpdate(sqlQuery);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-    }
 
 }
