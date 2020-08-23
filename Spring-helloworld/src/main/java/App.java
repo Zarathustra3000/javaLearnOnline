af6ae21fd410d16e1ddee1c576f68d57ab8c58e1
aleksandr <tolastoialeksandr@mail.ru>
2020-08-23 12:54:53 +0300

    add class "Cat"

--- Spring-helloworld/src/main/java/App.java
+++ Spring-helloworld/src/main/java/App.java
@@ -4,7 +4,15 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat catOne = (Cat) applicationContext.getBean(Cat.class);
+        Cat catTwo = (Cat) applicationContext.getBean(Cat.class);
+
+        System.out.println("equals one: " + (bean == bean2));
+        System.out.println("equals two: " + (catOne == catTwo));
 
         System.out.println(bean.getMessage());
     }
