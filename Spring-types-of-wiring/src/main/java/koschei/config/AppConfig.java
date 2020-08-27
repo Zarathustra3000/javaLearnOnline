e09cb50c208da0efbadb62a216865f51b8ce90a6
aleksandr <tolastoialeksandr@mail.ru>
2020-08-27 22:22:13 +0300

    bug fix

--- Spring-types-of-wiring/src/main/java/koschei/config/AppConfig.java
+++ Spring-types-of-wiring/src/main/java/koschei/config/AppConfig.java
@@ -1,9 +1,6 @@
 package koschei.config;
 
-import koschei.models.Egg6;
-import koschei.models.Island2;
-import koschei.models.Needle7;
-import koschei.models.Wood3;
+import koschei.models.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -20,5 +17,10 @@ public class AppConfig {
     @Bean
     public static Egg6 getEgg(Needle7 needle) {
         return new Egg6(needle);
+    }
+
+    @Bean
+    public static Deth8 getDeth8() {
+        return new Deth8();
     }
 }
